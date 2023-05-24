cbf27adc955cfdd0b82a71ac4048f0e73c753b25
gus955 <117479202+gus955@users.noreply.github.com>
2023-05-24 17:17:37 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://github.com/js-developer-portfolio-gus/blob/main/data/profile.json';
+    const url = 'https://gus955/js-developer-portfolio-gus/blob/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
